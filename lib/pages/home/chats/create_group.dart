432cdb489d258e186747e9f7cf20200622f68040
TuranTalayhan <tura0015@gmail.com>
2023-09-19 10:40:11 +0200

    Added save changes button to "create group" page

--- lib/pages/home/chats/create_group.dart
+++ lib/pages/home/chats/create_group.dart
@@ -2,8 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-import '../profile_view/profile_view_arguments.dart';
-
 class CreateGroup extends StatefulWidget {
   const CreateGroup({super.key});
 
@@ -22,93 +20,117 @@ class _CreateGroupState extends State<CreateGroup> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(elevation: 0),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: FutureBuilder(
-          future: contacts,
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return const Text("error");
-            }
+      body: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: FutureBuilder(
+                future: contacts,
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return const Text("error");
+                  }
 
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            var data = snapshot.data!.data();
-            if (data == null) {
-              return const Text("data is null");
-            }
-            if (data["contacts"] == null) {
-              return const Text("no contacts");
-            }
-            List list = data["contacts"];
-
-            final userStream = FirebaseFirestore.instance
-                .collection("users")
-                .where(FieldPath.documentId, whereIn: list)
-                .snapshots();
-
-            return StreamBuilder(
-              stream: userStream,
-              builder: (context, snapshot) {
-                if (snapshot.hasError) {
-                  return const Text("error");
-                }
-
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-                return ListView.builder(
-                  itemCount: snapshot.data!.size,
-                  itemBuilder: (context, index) {
-                    var document = snapshot.data!.docs[index];
-                    return InkWell(
-                      onTap: () {
-                        Navigator.pushNamed(context, "/profile_view",
-                            arguments: ProfileViewArguments(document.id));
-                      },
-                      child: ListTile(
-                        title: Text(document["displayName"]),
-                        subtitle: Text(document["status"]),
-                        leading: document["profilePicture"] == null
-                            ? Container(
-                                height: 50,
-                                width: 50,
-                                decoration: BoxDecoration(
-                                  color: Colors.teal[50]!.withOpacity(0.5),
-                                  shape: BoxShape.circle,
-                                ),
-                                child: const Center(child: Icon(Icons.person)))
-                            : CircleAvatar(
-                                backgroundImage:
-                                    NetworkImage(document["profilePicture"]),
-                                minRadius: 25,
-                                maxRadius: 25,
-                              ),
-                        trailing: Checkbox(
-                          onChanged: (value) {
-                            setState(() {
-                              if (checkedUsers.contains(index)) {
-                                checkedUsers.remove(index);
-                              } else {
-                                checkedUsers.add(index);
-                              }
-                            });
-                          },
-                          value: checkedUsers.contains(index),
-                        ),
-                      ),
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
                     );
-                  },
-                );
-              },
-            );
-          },
-        ),
+                  }
+                  var data = snapshot.data!.data();
+                  if (data == null) {
+                    return const Text("data is null");
+                  }
+                  if (data["contacts"] == null) {
+                    return const Text("no contacts");
+                  }
+                  List list = data["contacts"];
+
+                  final userStream = FirebaseFirestore.instance
+                      .collection("users")
+                      .where(FieldPath.documentId, whereIn: list)
+                      .snapshots();
+
+                  return StreamBuilder(
+                    stream: userStream,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasError) {
+                        return const Text("error");
+                      }
+
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      return ListView.builder(
+                        itemCount: snapshot.data!.size,
+                        itemBuilder: (context, index) {
+                          var document = snapshot.data!.docs[index];
+                          return ListTile(
+                            title: Text(document["displayName"]),
+                            subtitle: Text(document["status"]),
+                            leading: document["profilePicture"] == null
+                                ? Container(
+                                    height: 50,
+                                    width: 50,
+                                    decoration: BoxDecoration(
+                                      color: Colors.teal[50]!.withOpacity(0.5),
+                                      shape: BoxShape.circle,
+                                    ),
+                                    child:
+                                        const Center(child: Icon(Icons.person)))
+                                : CircleAvatar(
+                                    backgroundImage: NetworkImage(
+                                        document["profilePicture"]),
+                                    minRadius: 25,
+                                    maxRadius: 25,
+                                  ),
+                            trailing: Checkbox(
+                              onChanged: (value) {
+                                setState(() {
+                                  if (checkedUsers.contains(index)) {
+                                    checkedUsers.remove(index);
+                                  } else {
+                                    checkedUsers.add(index);
+                                  }
+                                });
+                              },
+                              value: checkedUsers.contains(index),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 20, 0, 100),
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width - 70,
+              child: TextButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    )),
+                    backgroundColor: MaterialStateProperty.all(Colors.teal)),
+                child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text("Save changes",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold)),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
