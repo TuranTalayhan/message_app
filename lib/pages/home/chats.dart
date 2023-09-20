0df6435841fe1cebf95afa456734f1f75af1428c
TuranTalayhan <tura0015@gmail.com>
2023-09-20 12:00:44 +0200

    Created chats page

--- lib/pages/home/chats.dart
+++ lib/pages/home/chats.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Chats extends StatefulWidget {
@@ -8,8 +10,59 @@ class Chats extends StatefulWidget {
 }
 
 class _ChatsState extends State<Chats> {
+  final _groupStream = FirebaseFirestore.instance
+      .collection("groups")
+      .where("members", arrayContains: FirebaseAuth.instance.currentUser!.uid)
+      .snapshots();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: StreamBuilder(
+          stream: _groupStream,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return const Text("error");
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return ListView.builder(
+              itemCount: snapshot.data!.size,
+              itemBuilder: (context, index) {
+                var document = snapshot.data!.docs[index];
+                return InkWell(
+                  onTap: () {},
+                  child: ListTile(
+                    title: Text(document["groupName"]),
+                    subtitle: Text(document["lastMessage"]),
+                    leading: document["groupPicture"] == null
+                        ? Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              color: Colors.teal[50]!.withOpacity(0.5),
+                              shape: BoxShape.circle,
+                            ),
+                            child: const Center(child: Icon(Icons.groups_3)))
+                        : CircleAvatar(
+                            backgroundImage:
+                                NetworkImage(document["groupPicture"]),
+                            minRadius: 25,
+                            maxRadius: 25,
+                          ),
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
   }
 }
